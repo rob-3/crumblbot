19b893085e561891c1d99bd3395fe7885319138e
Robert Boyd III <rob@robertcboyd.com>
2021-05-20 12:54:55 -0400

    Fix problem with changing buildId

--- bot.js
+++ bot.js
@@ -4,32 +4,35 @@ const fetch = require("node-fetch");
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log(process.env.DISCORD_KEY);
 });
 
-client.on("message", (message) => {
-  if (message.content === "!crumbl") {
-    fetch(
-      "https://crumblcookies.com/_next/data/-KVpzWBj2HBHdCW7haOpO/index.json"
-    )
-      .then((blob) => blob.json())
-      .then((data) => {
-        const cookies = data.pageProps.products.cookies;
-        console.log(cookies);
-        cookies
-          .filter(
-            (c) =>
-              c.name !== "Milk Chocolate Chip" && c.name !== "Chilled Sugar"
-          )
-          .forEach((cookie) => {
-            const embed = new Discord.MessageEmbed()
-              .setTitle(cookie.name)
-              .setDescription(cookie.description)
-              .setThumbnail(cookie.newImage);
-            message.channel.send(embed);
-          });
-      })
-      .catch((err) => console.error(err));
+const regex = /"buildId":"(.*?)"/;
+
+client.on("message", async (message) => {
+  try {
+    if (message.content === "!crumbl") {
+      const html = await fetch("https://crumblcookies.com").then((r) =>
+        r.text()
+      );
+      const buildId = html.match(regex)[1];
+      const data = await fetch(
+        `https://crumblcookies.com/_next/data/${buildId}/index.json`
+      ).then((blob) => blob.json());
+      const cookies = data.pageProps.products.cookies;
+      cookies
+        .filter(
+          (c) => c.name !== "Milk Chocolate Chip" && c.name !== "Chilled Sugar"
+        )
+        .forEach((cookie) => {
+          const embed = new Discord.MessageEmbed()
+            .setTitle(cookie.name)
+            .setDescription(cookie.description)
+            .setThumbnail(cookie.newImage);
+          message.channel.send(embed);
+        });
+    }
+  } catch (e) {
+    console.error(e);
   }
 });
 
