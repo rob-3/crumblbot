232ebefa11ef1f35dae77174fc7f948d17d6d2f2
Robert Boyd III <rob@robertcboyd.com>
2021-06-13 14:46:03 -0400

    Add transition bot message

--- bot.js
+++ bot.js
@@ -14,25 +14,15 @@ const regex = /"buildId":"(.*?)"/;
 client.on("message", async (message) => {
   try {
     if (message.content === "!crumbl") {
-      const html = await fetch("https://crumblcookies.com").then((r) =>
-        r.text()
-      );
-      const buildId = html.match(regex)[1];
-      const data = await fetch(
-        `https://crumblcookies.com/_next/data/${buildId}/index.json`
-      ).then((blob) => blob.json());
-      const cookies = data.pageProps.products.cookies;
-      cookies
-        .filter(
-          (c) => c.name !== "Milk Chocolate Chip" && c.name !== "Chilled Sugar"
-        )
-        .forEach((cookie) => {
-          const embed = new Discord.MessageEmbed()
-            .setTitle(cookie.name)
-            .setDescription(cookie.description)
-            .setThumbnail(cookie.image);
-          message.channel.send({ embeds: [embed] });
-        });
+      message.channel.send({
+        content:
+          "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-lqZLzifawhs%2FTzIi_iT7VRI%2FAAAAAAAAA3I%2F6qK3V38WkZI%2Fs1600%2FNo%2Bcookies%2Bfor%2Byou.jpg&f=1&nofb=1",
+      });
+      message.channel.send({
+        content: `Hi! I'm switching to Discord's new slash commands so that I can sleep when I'm not being used! These constant all-nighters are really getting to me. I'm staying up for few more weeks to warn unsuspecting users, but this command will stop working entirely in the future.
+
+**You can now summon me via the \`/crumbl\` slash command!**`,
+      });
     }
   } catch (e) {
     console.error(e);
